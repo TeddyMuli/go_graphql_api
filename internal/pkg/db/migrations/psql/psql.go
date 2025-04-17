8f1bad9505fe5dfc1b1476ade4f5bc88981829fa
TeddyMuli <tmuli974@gmail.com>
2025-04-17 22:22:20 +0300

    init db connection

--- internal/pkg/db/migrations/psql/psql.go
+++ internal/pkg/db/migrations/psql/psql.go
@@ -1,22 +1,75 @@
-package psql
+package database
 
 import (
 	"context"
+	"database/sql"
 	"log"
 
-	"github.com/golang-migrate/migrate"
-	"github.com/golang-migrate/migrate/database/mysql"
-	_ "github.com/golang-migrate/migrate/source/file"
-	"github.com/golang-migrate/migrate/v4/database/pgx/v5"
-	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v5"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v4/pgxpool"
+	_ "github.com/lib/pq" // PostgreSQL driver for database/sql
 )
 
-var databaseUrl = "postgres://username:password@localhost:5432/database_name"
+var Db *pgxpool.Pool
+var sqlDB *sql.DB
 
 func InitDB() {
-	conn, err := pgx.Connect(context.Background(), databaseUrl)
+	var connStr = "postgres://postgres:2025Muli!@localhost:5432/hackernews"
+
+	config, err := pgxpool.ParseConfig(connStr)
 	if err != nil {
-		
+		log.Panic(err)
+	}
+
+	db, err := pgxpool.ConnectConfig(context.Background(), config)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if err = db.Ping(context.Background()); err != nil {
+		log.Panic(err)
+	}
+
+	Db = db
+
+	sqlDB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if err = sqlDB.Ping(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func CloseDB() error {
+	if Db != nil {
+		Db.Close()
+	}
+	if sqlDB != nil {
+		sqlDB.Close()
+	}
+	return nil
+}
+
+func Migrate() {
+	driver, err := postgres.WithInstance(sqlDB, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://internal/pkg/db/migrations/psql",
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
 	}
 }
